297bc69dfca2ed1774a3cf177c738a34585db465
iscadefrango <50028196+iscadefrango@users.noreply.github.com>
2019-04-26 15:37:31 -0300

    trabalho inicial
    empurrando pro github o que já tem pronto

---
+++ resources/js/script.js
@@ -0,0 +1,37 @@
+(function(){
+	Conteudo = {
+		home: function() {
+
+		},
+		videos: {
+			guerra: {
+				cv7: function() {
+
+				},
+				cv8: function() {
+
+				},
+				cv9: function() {
+
+				},
+				cv10: function() {
+
+				}
+			},
+			farm: {
+				cv7: function() {
+
+				},
+				cv8: function() {
+
+				},
+				cv9: function() {
+
+				},
+				cv10: function() {
+
+				}
+			}
+		}
+	}
+})();
